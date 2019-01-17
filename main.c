7b01833bd304e1be7570caa3cb5fc83d590a7704
李财宝 <1825306263@qq.com>
2019-01-17 19:11:26 +0800

    基本功能

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
-
+#include <math.h>
+struct Point{
+    int x;
+    int y;
+}
 int main(){
+    int index =0;
+    struct Point arr[100];
+    printf("jinruguanlixitong\n");
 
-    printf("hello world!");
+    while(1){
+        printf("1---tianjia\n");
+        printf("2---shanchu\n");
+        printf("xianshisuoyouche\n");
+        printf("fanhuilizijizuijindeche\n");
+        printf("tuichu");
+
+        printf("xuanzegongneng\n");
+        int code;
+        scanf("%d",&code);
+    }
 
     return 0;
 
